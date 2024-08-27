438038ad12812a4ec235bc87826509430c0f159d
gl coin <gl2299g@gmail.com>
2024-08-27 16:06:21 +0800

    Merge Chia 2.4.2

--- chia/util/default_root.py
+++ chia/util/default_root.py
@@ -1,6 +1,10 @@
+from __future__ import annotations
+
 import os
 from pathlib import Path
 
 DEFAULT_ROOT_PATH = Path(os.path.expanduser(os.getenv("GOLD_ROOT", "~/.gold/mainnet"))).resolve()
 
 DEFAULT_KEYS_ROOT_PATH = Path(os.path.expanduser(os.getenv("GOLD_KEYS_ROOT", "~/.gold_keys"))).resolve()
+
+SIMULATOR_ROOT_PATH = Path(os.path.expanduser(os.getenv("GOLD_SIMULATOR_ROOT", "~/.gold/simulator"))).resolve()
